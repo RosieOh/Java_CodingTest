3277913b625e20ef0c39bc6cd444e2c76755a191
오태훈의 프로젝트 파일들 <20172207@gm.hannam.ac.kr>
2023-07-18 22:02:35 +0900

    자바 코딩테스트 문제 풀이

---
+++ src/sec1/java_10807.java
@@ -0,0 +1,16 @@
+package sec1;
+
+import java.util.Scanner;
+
+public class java_10807 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        sc.close();
+
+        int[]array = new 
+    }
+
+}
---
+++ src/sec1/java_2557.java
@@ -0,0 +1,8 @@
+package sec1;
+
+public class java_2557 {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+
+}
