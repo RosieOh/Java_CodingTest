6117a056f64acc19f8acd226a276cb1c865516ad
RosieOh <20172207@gm.hannam.ac.kr>
2024-04-21 20:13:23 +0900

    ✨FEAT : 입력한 정숫값의 부호(양수/음수/0) 판단하기2

---
+++ src/theory/JudgeABC1.java
@@ -0,0 +1,19 @@
+package theory;
+
+import java.util.Scanner;
+
+public class JudgeABC1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("정수를 입력하세요.: ");
+        int n = scanner.nextInt();
+
+        if(n == 1)
+            System.out.println("A");
+        else if (n == 2)
+            System.out.println("B");
+        else
+            System.out.println("C");
+    }
+}
