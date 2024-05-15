0a9791484437711f46c5311db23bf776f4716109
RosieOh <20172207@gm.hannam.ac.kr>
2024-05-15 17:15:24 +0900

    ✨FEAT : 백준 정렬 2606 문제 sol

---
+++ src/sec1/java_2606.java
@@ -0,0 +1,20 @@
+package sec1;
+
+import java.util.Scanner;
+
+public class java_2606 {
+    static int map[][];
+    static boolean visit[];
+    static int n, m, v;
+    static int count = 0;
+
+    static void dfs(int i) {
+        visit[i] = true;
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
