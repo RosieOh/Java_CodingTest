ec20b6da6a4ed73accb8c03823f73e2c1fee2e8a
RosieOh <20172207@gm.hannam.ac.kr>
2024-05-19 23:51:10 +0900

    ✨FEAT : 이분 탐색 실습

---
+++ src/theory/BinSearch.java
@@ -0,0 +1,14 @@
+package theory;
+
+public class BinSearch {
+    // 요솟수가 n개인 배열 a에서 key와 같은 요소를 이진 검색
+    static int binSearch(int[] a, int n, int key) {
+        int pl = 0;
+        int pr = n-1;
+
+        do {
+            int pc = (pl + pr) / 2;
+            if (a[pc] == key)
+        }
+    }
+}
