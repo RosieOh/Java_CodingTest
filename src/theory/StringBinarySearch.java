4f3b363693993fe633da88f0a965afd4c1c0d4f7
RosieOh <20172207@gm.hannam.ac.kr>
2024-05-21 02:36:11 +0900

    ✨FEAT : 문자열의 배열(자바의 키워드)에서 검색

---
+++ src/theory/StringBinarySearch.java
@@ -0,0 +1,24 @@
+package theory;
+
+import java.util.Scanner;
+
+// 문자열의 배열(자바의 키워드)에서 검색
+public class StringBinarySearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 자바에서 사용하는 키워드
+        String[] x = {
+                "abstract", "assert", "boolean", "break", "byte",
+                "case",     "catch",  "char",    "class", "const",
+                "continue", "default", "do",     "double", "else",
+                "enum",     "extends", "final",  "finally", "float",
+                "for",      "goto",    "if",     "implements", "import",
+                "instanceof", "int",   "interface", "long",   "native",
+                "new",      "package", "private", "protected", "public",
+                "return",   "short",   "static",  "strictfp", "super",
+                "switch",   "synchronized", "this", "throw", "throws",
+                ""
+        }
+    }
+}
