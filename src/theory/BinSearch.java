5279329361e5d2ab24d47f9137e67c87e8665b50
RosieOh <20172207@gm.hannam.ac.kr>
2024-05-21 02:34:14 +0900

    ✨FEAT : 이진 탐색 실습

--- src/theory/BinSearch.java
+++ src/theory/BinSearch.java
@@ -1,5 +1,8 @@
 package theory;
 
+import java.util.Scanner;
+
+// 이진 검색
 public class BinSearch {
     // 요솟수가 n개인 배열 a에서 key와 같은 요소를 이진 검색
     static int binSearch(int[] a, int n, int key) {
@@ -9,6 +12,42 @@ public class BinSearch {
         do {
             int pc = (pl + pr) / 2;
             if (a[pc] == key)
+                return pc;
+            else if (a[pc] < key)
+                pl = pc + 1;
+            else
+                pr = pc - 1;
+        } while (pl <= pr);
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("요솟수: ");
+        int num = scanner.nextInt();
+        int[] x = new int[num];
+
+        System.out.println("오름차순으로 입력하세요.");
+
+        System.out.print("x[0]: ");
+        x[0] = scanner.nextInt();
+
+        for(int i = 1; i < num; i++) {
+            do {
+                System.out.print("x[" + i + "]: ");
+                x[i] = scanner.nextInt();
+            } while (x[i] < x[i - 1]);
         }
+
+        System.out.print("검색할 값: ");
+        int ky = scanner.nextInt();
+        int idx = binSearch(x, num, ky);
+
+        if (idx == -1)
+            System.out.println("그 값의 요소가 없습니다.");
+        else
+            System.out.println("그 값은 x[" + idx + "]에 있습니다.");
     }
 }
